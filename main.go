b6bc285a4b24692f58258fa88f6db2b4f2b6d673
Dan Octavian <danoctavian91@gmail.com>
2016-02-12 14:34:53 -0800

    it works but idk why.wtf

--- main.go
+++ main.go
@@ -9,14 +9,15 @@ import (
   "os/user"
   "strconv"
   "strings"
-  "errors"
+  "github.com/dropbox/godropbox/errors"
+  "golang.org/x/crypto/ssh"
+  "bytes"
 )
 
 
 // FIXME: be able to configure the used SSH key
 func main() {
   fmt.Println("running digital ocean setup script..")
-
 
   pat, err := ReadTokenFromConfigFile()
   if err != nil {
@@ -38,22 +39,14 @@ func main() {
 
   log.Println(droplets)
 
+  //err = RemoveAllDroplets(client)
+  //err = createMasterSlaveDroplets(client, 5)
 
-
-  // err = RemoveAllDroplets(client)
-  err = createMasterSlaveDroplets(client, 0)
+  err = RunTentacularOnDroplets(GetTentacularDroplets(droplets))
   if err != nil {
     log.Println(err)
     return
   }
-
-  droplets, err = DropletList(client)
-  if err != nil {
-    log.Println(err)
-    return
-  }
-
-  log.Println(droplets)
 }
 
 func createMasterSlaveDroplets(client *godo.Client, slaveCount int) (err error) {
@@ -92,6 +85,7 @@ func createSmallDroplet(client *godo.Client, dropletName string, sshKeys []godo.
 }
 
 func RemoveAllDroplets(client *godo.Client) error {
+  log.Println("deleting all droplets...")
   droplets, err := DropletList(client)
   if err != nil {
     return err
@@ -167,7 +161,8 @@ func GetTentacularDroplets(droplets []godo.Droplet) (master *godo.Droplet, slave
   slaves = []godo.Droplet{}
   for _, droplet := range droplets {
     if IsMasterDroplet(&droplet) {
-      master = &droplet
+      current := droplet
+      master = &current
     } else if IsSlaveDroplet(&droplet) {
       slaves = append(slaves, droplet)
     }
@@ -186,6 +181,57 @@ func RunTentacularOnDroplets(master *godo.Droplet, slaves []godo.Droplet) (err e
     return errors.New("No slave nodes available.")
   }
 
+  nodeCount := 1 + len(slaves)
+
+  doneChan := make(chan error, nodeCount)
+
+  masterPubAddr, err := master.PublicIPv4()
+  if err != nil { err = errors.Wrap(err, "") ; return }
+
+  log.Println("running command")
+
+  // adding "&" at the end of commands so that it's ok to exit
+  go func() {
+    log.Println("running master proxy at " + masterPubAddr)
+    reString, err := RunRemoteCommand(masterPubAddr, setupCmd(RUN_PROXY_MASTER))
+    log.Println("master terminated with output " + reString)
+    if err != nil {
+      log.Println("master terminated with error.")
+      log.Println(err)
+    }
+    doneChan <- err
+  }()
+
+  masterPrivAddr, err := master.PrivateIPv4()
+  if err != nil { err = errors.Wrap(err, "") ; return }
+
+  slaveCommand := fmt.Sprintf(RUN_PROXY_SLAVE, masterPrivAddr)
+
+  for _, slave := range slaves {
+
+    slaveAddr, err := slave.PublicIPv4()
+    if err != nil {
+      fmt.Errorf("slave address could not be obtained. ignoring")
+      continue
+    }
+
+    go func() {
+      log.Println("running slave proxy at " + slaveAddr)
+      reString, err := RunRemoteCommand(slaveAddr, setupCmd(slaveCommand))
+      log.Println("slave terminated with output " + reString)
+      if err != nil {
+        log.Println("slave terminated with error.")
+        log.Println(err)
+      }
+      doneChan <- err
+    }()
+  }
+
+  log.Println("waiting for procs to finish..")
+  for i := 0; i < nodeCount; i++ {
+    <- doneChan
+  }
+
   return nil
 }
 
@@ -197,3 +243,51 @@ func IsSlaveDroplet(droplet *godo.Droplet) bool {
   return strings.HasPrefix(droplet.Name, "slave")
 }
 
+func setupCmd(cmd string) string {
+  return "(" + cmd + ");sleep 5"
+}
+
+// on a droplet assuming root user and ssh key at id_rsa.pub deployed
+func RunRemoteCommand(addr string, command string) (s string, err error) {
+
+  usr, err := user.Current()
+  if err != nil { err = errors.Wrap(err, "") ; return }
+
+  authMethod, err := PublicKeyFile(usr.HomeDir + "/.ssh/id_rsa")
+  if err != nil { err = errors.Wrap(err, "") ; return }
+
+  sshConfig := &ssh.ClientConfig{
+    User: "root",
+    Auth: []ssh.AuthMethod{authMethod},
+  }
+
+  conn, err := ssh.Dial("tcp", addr + ":22", sshConfig)
+  if err != nil { err = errors.Wrap(err, "ssh dial failed.") ; return }
+
+  defer conn.Close()
+
+  session, err := conn.NewSession()
+  if err != nil { err = errors.Wrap(err, "session failed."); return }
+
+  defer session.Close()
+
+  var stdoutBuf bytes.Buffer
+  session.Stdout = &stdoutBuf
+  err = session.Run(command)
+  if err != nil { err = errors.Wrap(err, "cmd failed."); return }
+
+  return stdoutBuf.String(), nil
+}
+
+func PublicKeyFile(file string) (auth ssh.AuthMethod, err error) {
+  buffer, err := ioutil.ReadFile(file)
+  if err != nil { err = errors.Wrap(err, "") ; return }
+
+  key, err := ssh.ParsePrivateKey(buffer)
+  if err != nil { err = errors.Wrap(err, "") ; return }
+
+  return ssh.PublicKeys(key), nil
+}
+
+const RUN_PROXY_MASTER = "docker run -p 8080:8080 -p 6666:6666 --name master --rm danoctavian/tentacular /go/bin/app --type=master"
+const RUN_PROXY_SLAVE = "docker run -p 8080:8080 --name slave --rm danoctavian/tentacular /go/bin/app --masterurl=\"http://%s:6666\""
